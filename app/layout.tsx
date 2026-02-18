2d4e5e788782c28ea4ac8094f7e77cc0c117b884
proyect2024-admin <abraham18eda@gmail.com>
2026-02-18 16:39:49 -0600

    se actualiza logo  y pixel de facebook

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1988912838328369');
+            fbq('track', 'PageView');
+          `}
+        </Script>
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-J2C1HKC0F6"
@@ -42,9 +57,22 @@ export default function RootLayout({
           `}
         </Script>
       </head>
-      <TranslationsProvider>
-        <body className={`${inter.variable} font-sans antialiased`}>{children}</body>
-      </TranslationsProvider>
+
+        <body className={`${inter.variable} font-sans antialiased`}>
+          {/* Noscript Meta Pixel */}
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src="https://www.facebook.com/tr?id=1988912838328369&ev=PageView&noscript=1"
+              alt=""
+            />
+          </noscript>
+          <TranslationsProvider>
+          {children}
+          </TranslationsProvider>
+        </body>
     </html>
   )
 }
--- components/header.tsx
+++ components/header.tsx
@@ -29,7 +29,7 @@ export function Header() {
     { label:  t('nav.contact'), href: "#contacto" },
   ]
 
-  const logo = { image: "/logo/logo-magicwebs-development.png"}
+  const logo = { image: "/logo/logo-magicwebs-depelover.png"}
 
   // Cierre del Menu en mobile al darle click fuera
   const headerRef  = useRef<HTMLDivElement>(null); // referencia al menú
